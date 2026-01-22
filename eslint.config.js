6f6e3aef49c8b23e28959490d73383a2b4eec82e
tuskinc <wasswaramzan@gmail.com>
2026-01-22 16:09:34 +0300

    Update eslint.config.js with the latest configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,24 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      // Enable type-aware lint rules
-      ...tseslint.configs.recommendedTypeChecked,
-      // Stricter type-aware rules (optional, but recommended for production)
-      ...tseslint.configs.strictTypeChecked,
-      // Stylistic rules with type-aware checks (optional)
-      ...tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:react-hooks/recommended'
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-])
+        tsconfigRootDir: new URL('.', import.meta.url).pathname
+      }
+    }
+  }
+]
